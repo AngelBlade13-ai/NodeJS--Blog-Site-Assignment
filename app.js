e1f92dfc800e62950c52765916f09b0d591004b4
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-02-03 13:11:54 -0700

    Part 1 completion

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const expressLayouts = require("express-ejs-layouts");
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const connectDB = require("./server/config/db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+
+const start = async () => {
+  await connectDB();
+  app.listen(PORT, () => console.log(`Server is running on on port: ${PORT}`));
+};
+
+start();
