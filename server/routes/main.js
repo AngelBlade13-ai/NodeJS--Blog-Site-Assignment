cc96bc48dca9777fb8e38c53af6041687807192f
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-02-03 15:21:15 -0700

    Part 2 Completiong

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,20 +2,77 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-//Home Page
-router.get("/", async (req, res) => {
-  const locals = {
-    title: "NodeJS Blog",
-    description:
-      "A Blog template application that will be used for your own use.",
-  };
-
+// Get Post by Id
+router.get("/post/:id", async (req, res) => {
   try {
-    const data = await Post.find().sort({ createdAt: "desc" });
-    res.render("index", { locals, data });
+    const { id } = req.params;
+    const data = await Post.findById(id);
+
+    const locals = {
+      title: data.title,
+      description:
+        "A Blog template application that will be used for your own use.",
+    };
+    res.render("post", { locals, data });
   } catch (error) {
     console.log(error);
   }
 });
+// Search Route
+router.post("/search", async (req, res) => {
+  try {
+    const locals = {
+      title: "Search",
+      description: "A blog template made with NodeJS and ExpressJS",
+    };
 
+    let searchTerm = req.body.searchTerm || "";
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z]/g, "");
+
+    const data = await Post.find({
+      $or: [
+        { title: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+        { body: { $regex: new RegExp(searchNoSpecialChar, "i") } },
+      ],
+    });
+
+    res.render("search", { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+});
+//pagination
+router.get("/", async (req, res) => {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog created with NodeJs, Express & MongoDb.",
+    };
+
+    let perPage = 3;
+    let page = parseInt(req.query.page || "1", 10);
+
+    const data = await Post.aggregate([{ $sort: { title: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    // Count is deprecated - please use countDocuments({}) instead
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+    const hasPrevPage = page > 1;
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+      prevPage: hasPrevPage ? page - 1 : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 module.exports = router;
