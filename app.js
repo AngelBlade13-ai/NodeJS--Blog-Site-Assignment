07d2b7f9a2750c5516d37304ca436e7086a0c58f
AngelBlade13-ai <bladenmckinlay@gmail.com>
2026-02-03 17:02:43 -0700

    Part 3 completion

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 require("dotenv").config();
 
+const cookieParser = require("cookie-parser");
+const MongoStoreModule = require("connect-mongo");
 const expressLayouts = require("express-ejs-layouts");
 const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const methodOverride = require("method-override");
+const session = require("express-session");
+
 const connectDB = require("./server/config/db");
+connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: (MongoStoreModule.default || MongoStoreModule).create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
+  }),
+);
 
 app.use(expressLayouts);
 app.use(express.static("public"));
@@ -16,10 +36,6 @@ app.set("layout", "./layouts/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
-const start = async () => {
-  await connectDB();
-  app.listen(PORT, () => console.log(`Server is running on on port: ${PORT}`));
-};
-
-start();
+app.listen(PORT, () => console.log(`server is running on port: ${PORT}`));
